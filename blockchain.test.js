0370d24a16f26abcf27c250070ce3d0384bda2fe
salarce <salarcedev@gmail.com>
2022-04-27 16:00:11 +0430

    isvalidchain method tests

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,7 +2,11 @@ const Block = require('./block');
 const Blockchain = require('./blockchain');
 
 describe('Blockchain', ()=>{
-    const blockchain = new Blockchain();
+    let blockchain = new Blockchain();
+
+    beforeEach(()=>{
+        blockchain = new Blockchain();
+    });
     
     it('containes a `chain` Array instance', ()=>{
         expect(blockchain.chain instanceof Array).toBe(true);
@@ -14,5 +18,43 @@ describe('Blockchain', ()=>{
         const newData = 'foo bar';
         blockchain.addBlock({data: newData});
         expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
+    });
+
+    describe('isValidChain()', ()=>{
+        describe('when the chain does not start with the genesis block', ()=>{
+            it('returns false', ()=>{
+                blockchain.chain[0] = {data: 'fake-genesis'};
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+            });
+        });
+
+        describe('when the chain does start with the genesis block and has multiple blocks', ()=>{
+
+            beforeEach(()=>{
+                blockchain.addBlock({data: 'one'});
+                blockchain.addBlock({data: 'two'});
+                blockchain.addBlock({data: 'three'});
+            });
+
+            describe('and a lastHash reference has changed', ()=>{
+                it('returns false', ()=>{
+                    blockchain.chain[2].lastHash = 'broken-lastHash';
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+                });
+            });
+
+            describe('and the chain contains a block with an invalid field', ()=>{
+                it('returns false', ()=>{
+                    blockchain.chain[2].data = 'changed-data';
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+                });
+            });
+
+            describe('and the chain does not contain any invald blocks', ()=>{
+                it('returns true', ()=>{
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+                });
+            });
+        });
     });
 });
