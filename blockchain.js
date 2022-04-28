a7870fe794c74a88afb36c87a9d9c760c51da48b
salarce <salarcedev@gmail.com>
2022-04-28 12:34:38 +0430

    deploy replacechain func

--- blockchain.js
+++ blockchain.js
@@ -28,6 +28,12 @@ class Blockchain{
 
         return true;
     }
+
+    replaceChain(chain){
+        if(chain.length <= this.chain.length) return;
+        if(!Blockchain.isValidChain(chain)) return;
+        this.chain = chain;
+    }
 }
 
 module.exports = Blockchain;
