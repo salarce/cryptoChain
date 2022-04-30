04ae0eaf13fa51aad881df6237e9108d91477f77
salarce <salarcedev@gmail.com>
2022-04-30 14:44:16 +0430

    update block test

--- block.test.js
+++ block.test.js
@@ -1,3 +1,4 @@
+const hexToBinary = require("hex-to-binary");
 const Block = require("./block");
 const {GENESIS_DATA, MINE_RATE} = require('./config');
 const cryptoHash = require('./crypto-hash');
@@ -57,7 +58,7 @@ describe("Block", ()=>{
         });
 
         it(('sets a `hash` that matches the difficulty criteria'), ()=>{
-            expect(minedBlock.hash.substring(0, minedBlock.difficulty)).toEqual('0'.repeat(minedBlock.difficulty));
+            expect(hexToBinary(minedBlock.hash).substring(0, minedBlock.difficulty)).toEqual('0'.repeat(minedBlock.difficulty));
         });
 
         it('adjusts the difficulty', ()=>{
