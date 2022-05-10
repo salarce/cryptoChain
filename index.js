4c05b15d6cb35a6e085d3edd480bdad56ac07c85
salarce <salarcedev@gmail.com>
2022-05-10 11:33:12 +0430

    sync nodes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Blockchain = require('./blockchain');
 const Pubsub = require('./pubsub');
 const tcpPortUsed = require('tcp-port-used');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -9,9 +10,9 @@ app.use(express.json());
 const blockchain = new Blockchain();
 const pubsub = new Pubsub({blockchain});
 
-setTimeout(() => {
-    pubsub.broadcastChain();
-}, 1000);
+// setTimeout(() => {
+//     pubsub.broadcastChain();
+// }, 1000);
 
 app.get('/api/blocks', (req, res)=>{
     res.json(blockchain.chain);
@@ -21,15 +22,25 @@ app.post('/api/mine', (req, res)=>{
     const {data} = req.body;
 
     blockchain.addBlock({data});
+    pubsub.broadcastChain();
     res.redirect('/api/blocks');
 });
 
+const rootPort = 3000;
 let PORT = 3000;
+
+const syncChains = async ()=>{
+    const response = await axios.get(`http://localhost:${rootPort}/api/blocks`);
+    blockchain.replaceChain(response.data);
+}
 
 tcpPortUsed.check(3000, '127.0.0.1')
 .then(function(inUse){
     if(inUse){
         PORT += Math.ceil(Math.random() * 1000)
     }
-    app.listen(PORT, ()=> console.log(`listening at localhost:${PORT}`));
+    app.listen(PORT, ()=>{
+        console.log(`listening at localhost:${PORT}`);
+        if(PORT !== rootPort) syncChains();
+    });
 })
