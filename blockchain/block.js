6ede53a9f6b77ac37d9da59dd26d1e8f4d28d4fc
salarce <salarcedev@gmail.com>
2022-05-10 19:47:55 +0430

    ver 1.3.0
    update same recipient
    fix cryptohash bug
    deploy update transaction func
    deploy createtransaction func
    deploy validtransaction func
    cryptohash refactor
    initialize inputput of transaction
    initialize output of transaction
    deploy sign & verifysignature func
    produce publickey with elliptic
    initialize wallet
    change file structure

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,5 +1,5 @@
-const { GENESIS_DATA, MINE_RATE } = require("./config");
-const cryptoHash = require('./crypto-hash');
+const { GENESIS_DATA, MINE_RATE } = require("../config");
+const {cryptoHash} = require('../util');
 const hexToBinary = require('hex-to-binary');
 
 class Block{
