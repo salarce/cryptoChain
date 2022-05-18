893d5c30dc5bf96e6ee0bfc5f6f950fb8d43b0ce
salarce <salarcedev@gmail.com>
2022-05-18 23:46:16 +0430

    signing data & verify signature test

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('./index');
+const {verifySignature} = require('../util');
 
 describe("Wallet", ()=>{
     let wallet;
@@ -12,5 +13,24 @@ describe("Wallet", ()=>{
     });
     it('has a `publicKey`', ()=>{
         expect(wallet).toHaveProperty('publicKey');
+    });
+
+    describe('signing data', ()=>{
+        const data = 'some-data';
+        
+        it('verifies a signature', ()=>{
+            expect(verifySignature({
+                publicKey: wallet.publicKey,
+                data,
+                signature: wallet.sign(data)
+            })).toBe(true);
+        });
+        it('does not verify an invalid signature', ()=>{
+            expect(verifySignature({
+                publicKey: wallet.publicKey,
+                data,
+                signature: new Wallet().sign(data)
+            })).toBe(false);
+        });
     });
 });
