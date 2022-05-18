24b247dd209a2ed037f304d77712f3aa56bdc6d0
salarce <salarcedev@gmail.com>
2022-05-18 23:19:20 +0430

    produce publickey with elliptic

--- wallet/index.js
+++ wallet/index.js
@@ -1,8 +1,12 @@
 const {STARTING_BALANCE} = require('../config');
+const {ec} = require('../util');
+
+const keyPair = ec.genKeyPair();
 
 class Wallet{
     constructor(){
         this.balance = STARTING_BALANCE;
+        this.publicKey = keyPair.getPublic().encode('hex');
     }
 }
 
