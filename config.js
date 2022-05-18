fad6197cd05935123bc6d9e0eac4c702cf29c8af
salarce <salarcedev@gmail.com>
2022-05-18 23:14:54 +0430

    initialize wallet

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const INITIAL_DIFFICULTY = 3;
 const MINE_RATE = 1000;
+const STARTING_BALANCE = 1000;
 
 const GENESIS_DATA = {
     timestamp: 1,
@@ -10,4 +11,4 @@ const GENESIS_DATA = {
     nonce: 0
 }
 
-module.exports = {GENESIS_DATA, MINE_RATE};
+module.exports = {GENESIS_DATA, MINE_RATE, STARTING_BALANCE};
