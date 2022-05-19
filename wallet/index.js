552e38368c563dd60ce0710b50bad1fecb19a21c
salarce <salarcedev@gmail.com>
2022-05-19 08:21:20 +0430

    deploy sign & verifysignature func

--- wallet/index.js
+++ wallet/index.js
@@ -1,12 +1,16 @@
 const {STARTING_BALANCE} = require('../config');
 const {ec} = require('../util');
-
-const keyPair = ec.genKeyPair();
+const cryptoHash = require('../util/crypto-hash');
 
 class Wallet{
     constructor(){
         this.balance = STARTING_BALANCE;
-        this.publicKey = keyPair.getPublic().encode('hex');
+        this.keyPair = ec.genKeyPair();
+        this.publicKey = this.keyPair.getPublic().encode('hex');
+    }
+
+    sign(data){
+        return this.keyPair.sign(cryptoHash(data));
     }
 }
 
