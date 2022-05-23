0f45df045901d9b3e9f9d341b19b6a4a22d6d205
salarce <salarcedev@gmail.com>
2022-05-23 09:30:56 +0430

    validtransaction test

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -53,4 +53,35 @@ describe("Transaction", ()=>{
             })).toBe(true);
         });
     });
+
+    describe("validTransaction", ()=>{
+        let errorMock;
+
+        beforeEach(()=>{
+            errorMock = jest.fn();
+            global.console.error = errorMock;
+        });
+
+        describe('when the transaction is valid', ()=>{
+            it('returns true', ()=>{
+                expect(Transaction.validTransaction(transaction)).toBe(true);
+            });
+        });
+        describe('when the transaction is invalid', ()=>{
+            describe('and a transaction outputMap value is invalid', ()=>{
+                it('returns false', ()=>{
+                    transaction.outputMap[senderWallet.publicKey] = 99999;
+                    expect(Transaction.validTransaction(transaction)).toBe(false);
+                    expect(errorMock).toHaveBeenCalled();
+                });
+            });
+            describe('and a transaction input signature is invalid', ()=>{
+                it('returns false', ()=>{
+                    transaction.input.signature = new Wallet().sign('data');
+                    expect(Transaction.validTransaction(transaction)).toBe(false);
+                    expect(errorMock).toHaveBeenCalled();
+                });
+            });
+        });
+    });
 });
