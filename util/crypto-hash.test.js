30e6c647eda7075f215ec1b178ec68df85852288
salarce <salarcedev@gmail.com>
2022-05-24 19:12:11 +0430

    update cryptohash test

--- util/crypto-hash.test.js
+++ util/crypto-hash.test.js
@@ -2,7 +2,7 @@ const cryptoHash = require('./crypto-hash');
 
 describe('cryptoHash()', ()=>{
     it('generates a SHA-256 hashed output', ()=>{
-        expect(cryptoHash('foo')).toEqual('2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae')
+        expect(cryptoHash('foo')).toEqual('b2213295d564916f89a6a42455567c87c3f480fcd7a1c15e220f17d7169a790b')
     });
     it('produces the same hash with the same input in any order', ()=>{
         expect(cryptoHash('one', 'two', 'three')).toEqual(cryptoHash('two', 'one', 'three'))
