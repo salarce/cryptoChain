e97998cc89cb4c7b59a3f08755cd52235116a0c0
salarce <salarcedev@gmail.com>
2022-05-27 18:48:51 +0430

    ver 1.4.0
    update transaction pools synchronization
    sync transaction pools
    transaction-pool-map api
    update existing transaction
    make transaction with api
    initialize transactionpool

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const Blockchain = require('./blockchain');
 const Pubsub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 const tcpPortUsed = require('tcp-port-used');
 const axios = require('axios');
+const { json } = require('express/lib/response');
 
 const app = express();
 app.use(express.json());
 
 const blockchain = new Blockchain();
-const pubsub = new Pubsub({blockchain});
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
+const pubsub = new Pubsub({blockchain, transactionPool});
 
 // setTimeout(() => {
 //     pubsub.broadcastChain();
@@ -16,6 +21,30 @@ const pubsub = new Pubsub({blockchain});
 
 app.get('/api/blocks', (req, res)=>{
     res.json(blockchain.chain);
+});
+
+app.get('/api/transaction-pool-map', (req, res)=>{
+    res.json(transactionPool.transactionMap);
+});
+
+app.post('/api/transact', (req, res)=>{
+    let {amount, recipient} = req.body;
+    amount = parseInt(amount);
+    let transaction = transactionPool.existingTransaction({
+        inputAddress: wallet.publicKey
+    });
+    try{
+        if(transaction){
+            transaction.update({senderWallet: wallet, recipient, amount});
+        }else{
+            transaction = wallet.createTransaction({recipient, amount});
+        }
+    }catch(error){
+        return res.json({type: 'error', message: error.message});
+    }
+    transactionPool.setTransaction(transaction);
+    pubsub.broadcastTransaction(transaction);
+    res.json({transaction});
 });
 
 app.post('/api/mine', (req, res)=>{
@@ -29,9 +58,12 @@ app.post('/api/mine', (req, res)=>{
 const rootPort = 3000;
 let PORT = 3000;
 
-const syncChains = async ()=>{
-    const response = await axios.get(`http://localhost:${rootPort}/api/blocks`);
+const syncOnConnect = async ()=>{
+    let response = await axios.get(`http://localhost:${rootPort}/api/blocks`);
     blockchain.replaceChain(response.data);
+
+    response = await axios.get(`http://localhost:${rootPort}/api/transaction-pool-map`);
+    transactionPool.setMap(response.data);
 }
 
 tcpPortUsed.check(3000, '127.0.0.1')
@@ -41,6 +73,6 @@ tcpPortUsed.check(3000, '127.0.0.1')
     }
     app.listen(PORT, ()=>{
         console.log(`listening at localhost:${PORT}`);
-        if(PORT !== rootPort) syncChains();
+        if(PORT !== rootPort) syncOnConnect();
     });
 })
