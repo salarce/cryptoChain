7353a965d4f25607d111979eab6b2538839913fb
salarce <salarcedev@gmail.com>
2022-05-30 07:45:04 +0430

    deploy validtransaction func

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,3 +1,5 @@
+const Transaction = require("./transaction");
+
 class TransactionPool{
     constructor(){
         this.transactionMap = {};
@@ -14,6 +16,12 @@ class TransactionPool{
 
     setMap(transactionMap){
         this.transactionMap = transactionMap;
+    }
+
+    validTransactions(){
+        return Object.values(this.transactionMap).filter(
+            transaction => Transaction.validTransaction(transaction)
+        );
     }
 }
 
