0ab9189e561cc2803611d1f2311096522c84bf26
salarce <salarcedev@gmail.com>
2022-05-30 09:33:48 +0430

    deployrewardtransaction func

--- config.js
+++ config.js
@@ -2,6 +2,9 @@ const INITIAL_DIFFICULTY = 3;
 const MINE_RATE = 1000;
 const STARTING_BALANCE = 1000;
 
+const REWARD_INPUT = {address: '*authorized-reward*'};
+const MINING_REWARD = 50;
+
 const GENESIS_DATA = {
     timestamp: 1,
     lastHash: '_____',
@@ -11,4 +14,4 @@ const GENESIS_DATA = {
     nonce: 0
 }
 
-module.exports = {GENESIS_DATA, MINE_RATE, STARTING_BALANCE};
+module.exports = {GENESIS_DATA, MINE_RATE, STARTING_BALANCE, REWARD_INPUT, MINING_REWARD};
--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,11 +1,12 @@
 const {v1} = require('uuid');
 const {verifySignature} = require('../util'); 
+const {REWARD_INPUT, MINING_REWARD} = require('../config');
 
 class Transaction{
-    constructor({senderWallet, recipient, amount}){
+    constructor({senderWallet, recipient, amount, outputMap, input}){
         this.id = v1();
-        this.outputMap = this.createOutputMap({senderWallet, recipient, amount});
-        this.input = this.createInput({senderWallet, outputMap: this.outputMap});
+        this.outputMap = outputMap || this.createOutputMap({senderWallet, recipient, amount});
+        this.input = input || this.createInput({senderWallet, outputMap: this.outputMap});
     }
 
     createOutputMap({senderWallet, recipient, amount}){
@@ -55,6 +56,13 @@ class Transaction{
         this.outputMap[senderWallet.publicKey] = this.outputMap[senderWallet.publicKey] - amount;
         
         this.input = this.createInput({senderWallet, outputMap: this.outputMap});
+    }
+
+    static rewardTransaction({minerWallet}){
+        return new this({
+            input: REWARD_INPUT,
+            outputMap: {[minerWallet.publicKey] : MINING_REWARD}
+        });
     }
 }
 
