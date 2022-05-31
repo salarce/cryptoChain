3102708d81a424972f55c8c0b780df4f05a0abd9
salarce <salarcedev@gmail.com>
2022-05-31 15:29:20 +0430

    deploy clear func

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -23,6 +23,21 @@ class TransactionPool{
             transaction => Transaction.validTransaction(transaction)
         );
     }
+
+    clear(){
+        this.transactionMap = {};
+    }
+
+    clearBlockchainTransactions({chain}){
+        for(let i=0; i<chain.length; i++){
+            const block = chain[i];
+            for(let transaction of block.data){
+                if(this.transactionMap[transaction.id]){
+                    delete this.transactionMap[transaction.id];
+                }
+            }
+        }
+    }
 }
 
 module.exports = TransactionPool;
