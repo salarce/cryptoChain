c59aa5a92d85e3960204d1264fd6905af46a0ac2
salarce <salarcedev@gmail.com>
2022-05-31 15:23:53 +0430

    clear method tests

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -2,6 +2,7 @@ const TransactionPool = require('./transaction-pool');
 const Transaction = require('./transaction');
 const Wallet = require('./index');
 const { validTransaction } = require('./transaction');
+const Blockchain = require('../blockchain');
 
 describe('TransactionPool', ()=>{
     let transactionPool, transaction, senderWallet;
@@ -60,6 +61,37 @@ describe('TransactionPool', ()=>{
         it('logs errors for the invalid transactions', ()=>{
             transactionPool.validTransactions();
             expect(errorMock).toHaveBeenCalled();
+        });
+    });
+
+    describe("clear()", ()=>{
+        it('clears the t;ransactions', ()=>{
+            transactionPool.clear();
+            expect(transactionPool.transactionMap).toEqual({});
+        });
+    });
+
+    describe("clearBlockchainTransactions()", ()=>{
+        it('clears the pool of any existing blockchain transactions', ()=>{
+            const blockchain = new Blockchain();
+            const expectedTransactionMap = {};
+
+            for(let i=0; i<6; i++){
+                const transaction = new Wallet().createTransaction({
+                    recipient: 'foo',
+                    amount: 20
+                });
+                transactionPool.setTransaction(transaction);
+
+                if(i%2 == 0){
+                    blockchain.addBlock({data: [transaction]});
+                }else{
+                    expectedTransactionMap[transaction.id] = transaction;
+                }
+            }
+
+            transactionPool.clearBlockchainTransactions({chain: blockchain.chain});
+            expect(transactionPool.transactionMap).toEqual(expectedTransactionMap);
         });
     });
 });
