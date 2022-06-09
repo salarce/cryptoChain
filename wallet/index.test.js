845ff92d3d5b54e0dd2b5b4814ddc7493750538c
salarce <salarcedev@gmail.com>
2022-06-09 09:03:53 +0430

    update createtransaction test for balance check

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -65,6 +65,23 @@ describe("Wallet", ()=>{
                 expect(transaction.outputMap[recipient]).toEqual(amount);
             });
         });
+
+        describe('and the chain is passed', ()=>{
+            it('calls `wallet.calculateBalance`', ()=>{
+                const calculateBalanceMock = jest.fn();
+                const originalCalculateBalance = Wallet.calculateBalance;
+                Wallet.calculateBalance = calculateBalanceMock;
+
+                wallet.createTransaction({
+                    recipient: 'foo',
+                    amount: 10,
+                    chain: new Blockchain().chain
+                });
+
+                expect(calculateBalanceMock).toHaveBeenCalled();
+                Wallet.calculateBalance = originalCalculateBalance;
+            });
+        });
     });
 
     describe("calculateBalance()", ()=>{
