976c908c8ab5d305a3b924e88d737c7c4849e0b2
salarce <salarcedev@gmail.com>
2022-06-09 17:19:56 +0430

    fix calculatebalance algorithm

--- wallet/index.js
+++ wallet/index.js
@@ -30,20 +30,24 @@ class Wallet{
     }
 
     static calculateBalance({chain, address}){
+        let hasCondectedTransaction = false;
         let outputTotal = 0;
 
-        for(let i=1; i<chain.length; i++){
+        for(let i=chain.length-1; i>0; i--){
             const block = chain[i];
 
             for(let transaction of block.data){
+                if(transaction.input.address === address) hasCondectedTransaction = true;
                 const addressOutput = transaction.outputMap[address];
                 if(addressOutput){
                     outputTotal += addressOutput;
                 }
             }
+
+            if(hasCondectedTransaction) break;
         }
 
-        return STARTING_BALANCE + outputTotal;
+        return hasCondectedTransaction ? outputTotal : STARTING_BALANCE + outputTotal;
     }
 }
 
