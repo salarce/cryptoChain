b9bb81b9095d4864f83f09747e1cc96719977d74
salarce <salarcedev@gmail.com>
2022-06-14 20:37:36 +0430

    adding flag to replacechain

--- blockchain/index.js
+++ blockchain/index.js
@@ -34,7 +34,7 @@ class Blockchain{
         return true;
     }
 
-    replaceChain(chain, onSuccess){
+    replaceChain(chain, validTransactions, onSuccess){
         if(chain.length <= this.chain.length){
             console.error('the incoming chain must be longer');
             return;
@@ -43,7 +43,7 @@ class Blockchain{
             console.error('the incoming chain must be valid');
             return;
         }
-        if(!this.validTransactionData({chain})){
+        if(validTransactions && !this.validTransactionData({chain})){
             console.error('The incoming chain has invalid data');
             return;
         }
