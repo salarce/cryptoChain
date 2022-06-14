3af1933131d52c27054c8892edb97b3fda4601b3
salarce <salarcedev@gmail.com>
2022-06-14 12:29:15 +0430

    ver 1.6.0
    adding flag to replacechain
    using of validTransactionData in replaceChain
    validtransactiondata update for multiple transactions
    validtransactiondata update for amount field
    deploy validTransactionData func

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,5 +1,8 @@
 const Block = require('./block');
 const {cryptoHash} = require('../util');
+const {REWARD_INPUT, MINING_REWARD} = require('../config');
+const Wallet = require('../wallet');
+const Transaction = require('../wallet/transaction');
 
 class Blockchain{
     constructor(){
@@ -31,7 +34,7 @@ class Blockchain{
         return true;
     }
 
-    replaceChain(chain, onSuccess){
+    replaceChain(chain, validTransactions, onSuccess){
         if(chain.length <= this.chain.length){
             console.error('the incoming chain must be longer');
             return;
@@ -40,9 +43,58 @@ class Blockchain{
             console.error('the incoming chain must be valid');
             return;
         }
+        if(validTransactions && !this.validTransactionData({chain})){
+            console.error('The incoming chain has invalid data');
+            return;
+        }
         if(onSuccess) onSuccess();
         console.log('replacing chain with: ', chain);
         this.chain = chain;
+    }
+
+    validTransactionData({chain}){
+        for(let i=1; i<chain.length; i++){
+            const block = chain[i];
+            const transactionSet = new Set();
+            let rewardTransactionCount = 0;
+
+            for(let transaction of block.data){
+                if(transaction.input.address === REWARD_INPUT.address){
+                    rewardTransactionCount++;
+                    if(rewardTransactionCount > 1){
+                        console.error('Miner reward exceed limit');
+                        return false;
+                    }
+                    if(Object.values(transaction.outputMap)[0] !== MINING_REWARD){
+                        console.error('Miner reward is invalid');
+                        return false;
+                    }
+                }else{
+                    if(!Transaction.validTransaction(transaction)){
+                        console.error('Invalid transaction');
+                        return false;
+                    }
+                    const trueBalance = Wallet.calculateBalance({
+                        chain: this.chain,
+                        address: transaction.input.address
+                    });
+
+                    if(transaction.input.amount !== trueBalance){
+                        console.error('Invalid input amount');
+                        return false;
+                    }
+
+                    if(transactionSet.has(transaction)){
+                        console.error('an identical transaction apears more than one in the block');
+                        return false;
+                    }else{
+                        transactionSet.add(transaction);
+                    }
+                }
+            }
+        }
+
+        return true;
     }
 }
 
